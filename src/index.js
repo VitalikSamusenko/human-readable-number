774a8182bcb9d1b764f7c214d3b645b860590950
Vitalik Samusenko <vitaliksamusenko1997@gmail.com>
2022-01-12 09:20:51 +0300

    feat: add result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
+
+  let oneSymbol = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  let twoSymbol = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let threeSymbol = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  let hundreds = Math.floor(number/100);
+  let dozens = number % 100;
+  let units = dozens % 10;
+  let result = [];
+
+  if (number === 0)
+    return 'zero';
+  if (hundreds > 0){
+    result.push(oneSymbol[hundreds] + ' hundred');
+  }
+  if (dozens >=20){
+    result.push(threeSymbol[Math.floor(dozens/10)]);
+    if(units !== 0)
+    {
+        result.push(oneSymbol[units]);
+    }
+  }else if(dozens >= 10) result.push(twoSymbol[dozens % 10]);
+  else if(dozens !== 0) result.push(oneSymbol[units]);
+  return result.join(' ');
 }
+
+
